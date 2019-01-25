7f907842b0a674e240c708c521f9aa00146cdb5d
Matt Pilcher <matthew.pilcher@holidayextras.com>
2019-01-25 12:53:27 +0000

    allow cors, get data on client

--- client/containers/Base.js
+++ client/containers/Base.js
@@ -3,12 +3,34 @@ import React, { Component } from 'react'
 class Base extends Component {
   constructor() {
     super()
+    this.state = {
+      users: []
+    }
+  }
+
+  async componentWillMount() {
+    const users =  await fetch('http://localhost:3000/users')
+      .then(response => response.json())
+
+    this.setState(() => {
+      return {users: users}
+    })
   }
 
   render() {
+    console.log(this.state.users)
     return (
       <div>
         <h1>Welcome...</h1>
+        <hr />
+        {this.state.users.map((user, i) => {
+          return (
+            <div>
+              <h4>Name: {user.name}</h4>
+              <p>User: {user.username}</p>
+            </div>
+          )
+        })}
       </div>
     )
   }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,17 @@ const express = require('express')
 const app = express()
 const users = require('./endpoints/users')
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => res.send('Server running...'))
 app.post('/', function (req, res) {
   res.send('Got a POST request')
 })
+
+// Allow cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/users', users)
 
